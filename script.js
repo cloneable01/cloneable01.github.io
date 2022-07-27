2fe49686cf604d79d8fb5b094037890f41eeca4b
cloneable01 <kevinkidley@gmail.com>
2022-07-27 21:42:49 +0700

    add new resposive navbar

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const hamburger = document.getElementById ('hamburger')
 const NavUl = document.getElementById ('ul_main_nav')
+var isActive = false
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle ('show');
-    ul_main_nav.classList.toggle('show');
+
+    if (isActive){
+       isActive = false
+       hamburger.classList.remove('is-active');
+       ul_main_nav.classList.remove('show');
+    }    else {
+        isActive = true
+        hamburger.classList.add('is-active');
+         ul_main_nav.classList.add('show');
+    }
+
 })
