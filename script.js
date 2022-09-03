8dcc747c8c9128a60e51c036a535b376bee8e610
cloneable01 <kevinkidley@gmail.com>
2022-09-03 22:44:32 +0700

    styling update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const navBar = document.getElementById ('navBar')
 const hamburger = document.getElementById ('hamburger')
 const NavUl = document.getElementById ('ul_main_nav')
 const HamSpan = document.getElementById('hamburger-box')
@@ -7,10 +8,12 @@ var isActive = false
 document.onclick = function(clickevent){
 
     if ( clickevent.target.id !== 'NavUl'  && clickevent.target.id !== 'hamburger' 
-    && clickevent.target.id !== 'hamburger-box' && clickevent.target.id !== 'bar')
+    && clickevent.target.id !== 'hamburger-box' && clickevent.target.id !== 'bar'
+    && clickevent.target.id !== 'navBar')
     {
         hamburger.classList.remove('is-active');
         NavUl.classList.remove('show');
+        navBar.style.backgroundColor = 'transparent';
     }
 }
 
@@ -20,9 +23,16 @@ hamburger.addEventListener('click', () => {
        isActive = false
        hamburger.classList.remove('is-active');
        NavUl.classList.remove('show');
+       navBar.style.width = '0';
+       navBar.style.backgroundColor = 'transparent';
     }    else {
         isActive = true
         hamburger.classList.add('is-active');
-         NavUl.classList.add('show');
+        NavUl.classList.add('show');
+        navBar.style.backgroundColor = '#746C70';
+        navBar.style.width = '120px';
+        NavUl.style.backgroundColor = '#746C70';
+        NavUl.style.width = '120px';
+        NavUl.style.height = '90vh';
     }
 })
