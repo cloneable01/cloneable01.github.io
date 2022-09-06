1d6e1bd333e1cc1c16b93e462ad5dd623524cc5a
cloneable01 <kevinkidley@gmail.com>
2022-09-06 19:20:42 +0700

    js simplified

--- script.js
+++ script.js
@@ -5,35 +5,27 @@ const HamSpan = document.getElementById('hamburger-box')
 const bar = document.getElementById ('bar')
 var isActive = false
 
-document.onclick = function(clickevent){
+function toggleNavBar(){
 
-    if ( clickevent.target.id !== 'NavUl'  && clickevent.target.id !== 'hamburger' 
-    && clickevent.target.id !== 'hamburger-box' && clickevent.target.id !== 'bar'
-    && clickevent.target.id !== 'navBar')
-    {
+    if (isActive){
+        isActive = false
         hamburger.classList.remove('is-active');
         NavUl.classList.remove('show');
-        navBar.style.backgroundColor = 'transparent';
-        isActive = false;
+        navBar.style.width = '0';
+     }    else {
+         isActive = true
+         hamburger.classList.add('is-active');
+         NavUl.classList.add('show');
+         navBar.style.backgroundColor = '#746C70';
+         navBar.style.width = '120px';
+         navBar.style.height = '100vh';
+     }
+}
+
+document.onclick = function(clickevent){
+    if( !['NavUl', 'hamburger', 'hamburger-box', 'bar', 'navBar' ].includes(clickevent.target.id) && isActive) {
+        toggleNavBar()
     }
 }
 
-hamburger.addEventListener('click', () => {
-
-    if (isActive){
-       isActive = false
-       hamburger.classList.remove('is-active');
-       NavUl.classList.remove('show');
-       navBar.style.width = '0';
-       navBar.style.backgroundColor = 'transparent';
-    }    else {
-        isActive = true
-        hamburger.classList.add('is-active');
-        NavUl.classList.add('show');
-        navBar.style.backgroundColor = '#746C70';
-        navBar.style.width = '120px';
-        NavUl.style.backgroundColor = '#746C70';
-        NavUl.style.width = '120px';
-        NavUl.style.height = '90vh';
-    }
-})
+hamburger.addEventListener('click', toggleNavBar)
