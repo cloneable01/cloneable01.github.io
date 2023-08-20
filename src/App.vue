6d9cb43c95375fccbdfe04a5af3e5c3524a3b39e
kevin <kevinkidley@gmail.com>
2023-08-21 04:59:18 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,6 @@ import styles from "./style.css?inline"
 import Navbar from "./components/navbar.vue"
 import Home from "./components/Home.vue"
 import About from "./components/About.vue"
-import Vue from "vue";
 
 export default {
   components: {
