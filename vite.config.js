6a4a61b702fa292a16598d6abb94c077ffaee852
kevin <kevinkidley@gmail.com>
2023-08-21 04:49:26 +0700

    Trigger rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from 'vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/cloneable01.github.io/',
+  base: '/cloneable01.github.io',
   server: {
     host: true
   }
